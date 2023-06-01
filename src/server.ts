6bc324420bc46e06c74aeb8370c75b2f2d01f67a
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-01 23:46:08 +0600

    Logger created using winston npm, success and error logs separated and stored in a logs directory file, .eslintrc rules issue solved

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,20 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { logger, errorLogger } from './shared/logger'
 
 // Database connection
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database connection successful`)
+    logger.info(`Database connection successful`)
 
     // App listening
     app.listen(config.port, () => {
-      console.log(`Server is listening on port ${config.port}`)
+      logger.info(`Server is listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(`Failed to connect database`, err)
+    errorLogger.error(`Failed to connect database`, err)
   }
 }
 // Calling the function
