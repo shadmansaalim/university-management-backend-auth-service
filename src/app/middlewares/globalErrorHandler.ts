7539e4deaf02c4a9af86f67a303e9737c477597b
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-04 12:21:30 +0600

    [DONE]mongoose validation error handle, instances of ApiError custom class errors handle, regular errors handle, code organised

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,23 +1,39 @@
 //Imports
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
 import config from '../../config'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import mongoose from 'mongoose'
+import handleValidationError from '../../errors/handleValidationError'
+import ApiError from '../../errors/ApiError'
 
 // Global Error Handler Function to create a specified format for different type of errors
-const globalErrorHandler = (
-  err: mongoose.Error.ValidationError,
+const globalErrorHandler: ErrorRequestHandler = (
+  err: Error | mongoose.Error.ValidationError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   // Initializing defaults
-  const statusCode = 500
-  const message = 'Something went wrong!'
-  const errorMessages: Array<IGenericErrorMessage> = []
+  let statusCode = 500
+  let message = 'Something went wrong!'
+  let errorMessages: Array<IGenericErrorMessage> = []
 
-  if (err?.name === 'ValidationError') {
-    // Handle validation error
+  // Handle validation error
+  if (err instanceof mongoose.Error.ValidationError) {
+    const formattedError = handleValidationError(err)
+    // Destructuring
+    ;({ statusCode, message, errorMessages } = formattedError)
+  }
+  // Handle errors that are instances of our custom ApiError class
+  else if (err instanceof ApiError) {
+    statusCode = err?.statusCode
+    message = err?.message
+    errorMessages = err?.message ? [{ path: '', message: message }] : []
+  }
+  // Handle regular errors that are instances of Error
+  else if (err instanceof Error) {
+    message = err?.message
+    errorMessages = err?.message ? [{ path: '', message: message }] : []
   }
 
   res.status(statusCode).json({
---
+++ src/interfaces/common.ts
@@ -0,0 +1,9 @@
+//Imports
+import { IGenericErrorMessage } from './error'
+
+// Error response format
+export type IGenericErrorResponse = {
+  statusCode: number
+  message: string
+  errorMessages: IGenericErrorMessage[]
+}
