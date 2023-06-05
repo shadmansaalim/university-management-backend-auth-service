8e949ebc70ca5d15f286c613337a05e0dcbc9f52
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-05 14:08:21 +0600

    [DONE] Created Academic Semester Interface and Model

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,41 @@
+// Imports
+import { Schema, model } from 'mongoose';
+import {
+  IAcademicSemester,
+  AcademicSemesterModel,
+} from './academicSemester.interface';
+
+// Academic Semester Schema
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Academic Semester Model
+export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
+  'AcademicSemester',
+  academicSemesterSchema
+);
