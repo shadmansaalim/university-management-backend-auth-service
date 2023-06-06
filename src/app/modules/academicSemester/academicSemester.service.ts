e1fdc7a5bd16aa4c69786090fbbd9d54b29ff65a
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-06 11:46:37 +0600

    [DONE] Semester Code Validation completed, consistency relation built between Academic Semester Title and Code.

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,11 +1,20 @@
 // Imports
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
+import { AcademicSemesterConstants } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
 
 // Create Semester Function
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  // Checking whether the format of payload is following the relation consistency of Title and Code
+  if (
+    payload?.code !== AcademicSemesterConstants.TitleCodeMapper[payload?.title]
+  ) {
+    throw new ApiError(400, 'Invalid Semester Code Provided');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
