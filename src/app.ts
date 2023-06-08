b2aad6f4d6b01caa00b78bf8870be2334d5fcdd8
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-08 20:38:13 +0600

    [DONE] NOT FOUND Route handling completed, code refactored/improved

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 // Imports
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 // Application Routes
 import routes from './app/routes';
+import httpStatus from 'http-status';
 
 // Express App
 const app: Application = express();
@@ -21,5 +22,21 @@ app.use('/api/v1', routes);
 
 // Global Error Handler
 app.use(globalErrorHandler);
+
+// Handle NOT FOUND Route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found.',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Route doesn't exists.",
+      },
+    ],
+  });
+
+  next();
+});
 
 export default app;
