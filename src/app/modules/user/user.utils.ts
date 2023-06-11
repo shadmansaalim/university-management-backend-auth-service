67fbc36675976b95463b0f90f789326630d653df
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-11 23:51:02 +0600

    Generate Student Id function implemented and code modified according to requirement analaysis

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,8 +1,9 @@
 // Imports
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { User } from './user.model';
 
-// Function to find last user id
-export const findLastUserId = async () => {
+// Function to find last student id
+export const findLastStudentId = async () => {
   // Finding One User and taking id only using filed filtering
   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
     .sort({
@@ -13,16 +14,29 @@ export const findLastUserId = async () => {
   return lastUser?.id;
 };
 
-// Function to generate user id
-export const generateUserId = async () => {
-  // Getting last user id and keeping it in defaultId variable if not storing default id which is for the first user in the database
-  const defaultId = (await findLastUserId()) || (0).toString().padStart(5, '0');
+// Function to generate student id
+export const generateStudentId = async (
+  academicSemester: IAcademicSemester
+): Promise<string> => {
+  // Student Last two digits of academic semester year
+  const studentLastTwoDigitsOfAcademicSemesterYear =
+    academicSemester.year.substring(2);
+
+  // Student academic semester year code
+  const studentAcademicSemesterCode = academicSemester.code;
+
+  // Getting last student id and keeping it in defaultId variable if not storing default id which is for the first user in the database
+  const defaultId =
+    (await findLastStudentId()) || (0).toString().padStart(5, '0');
 
   // Increment defaultId by 1
   const currentId = parseInt(defaultId) + 1;
 
   // Format current id to add starting '0's
-  const formattedCurrentId = currentId.toString().padStart(5, '0');
+  const formattedCurrentId =
+    studentLastTwoDigitsOfAcademicSemesterYear +
+    studentAcademicSemesterCode +
+    currentId.toString().padStart(5, '0');
 
   return formattedCurrentId;
 };
---
+++ src/enums/users.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+// Enum User Roles
+export enum ENUM_USER_ROLES {
+  SUPER_ADMIN = 'super_admin',
+  ADMIN = 'admin',
+  STUDENT = 'student',
+  FACULTY = 'faculty',
+}
