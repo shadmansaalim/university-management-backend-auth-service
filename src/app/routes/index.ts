cd01b3d4c3f47ed6ffe4314b04aed1a9009ed927
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-11 12:11:44 +0600

    Academic Faculty Router name fixed and router added to app module routes list

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { IModuleRoute } from './route.interface';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 // Express router
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 const moduleRoutes: IModuleRoute[] = [
   { path: '/users', route: UserRoutes },
   { path: '/academic-semesters', route: AcademicSemesterRoutes },
+  { path: '/academic-faculties', route: AcademicFacultyRoutes },
 ];
 
 // Application Routes
