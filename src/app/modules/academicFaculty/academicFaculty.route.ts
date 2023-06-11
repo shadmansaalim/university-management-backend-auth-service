f964ecff94db2902398375826d725162d2563b1c
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-11 13:23:43 +0600

    ZOD schema implemented in Academic Facculty route level to handle errors and prevent errors in route level

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,6 +1,8 @@
 // Imports
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 // Express router
 const router = express.Router();
@@ -12,10 +14,15 @@ router.get('/', AcademicFacultyController.getAllFaculties);
 
 router.post(
   '/create-academic-faculty',
+  validateRequest(AcademicFacultyValidation.createAcademicFacultyZodSchema),
   AcademicFacultyController.createFaculty
 );
 
-router.patch('/:id', AcademicFacultyController.updateSingleFaculty);
+router.patch(
+  '/:id',
+  validateRequest(AcademicFacultyValidation.updateAcademicFacultyZodSchema),
+  AcademicFacultyController.updateSingleFaculty
+);
 
 router.delete('/:id', AcademicFacultyController.deleteSingleFaculty);
 
