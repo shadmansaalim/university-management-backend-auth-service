4b4160858fec9a18873d9355675e57a051181281
shadmansaalim <shadmansaalim999@gmail.com>
2023-06-20 23:49:09 +0600

    Auth route created, POST login

---
+++ src/app/modules/auth/auth.route.ts
@@ -0,0 +1,10 @@
+// Imports
+import express from 'express';
+
+// Express router
+const router = express.Router();
+
+// API Endpoints
+router.post('/login');
+
+export const AuthRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,6 +9,7 @@ import { StudentRoutes } from '../modules/student/student.route';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
 import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
 import { AdminRoutes } from '../modules/admin/admin.route';
+import { AuthRoutes } from '../modules/auth/auth.route';
 
 // Express router
 const router = express.Router();
@@ -19,6 +20,7 @@ const moduleRoutes: IModuleRoute[] = [
   { path: '/students', route: StudentRoutes },
   { path: '/faculties', route: FacultyRoutes },
   { path: '/admins', route: AdminRoutes },
+  { path: '/auth', route: AuthRoutes },
   { path: '/academic-semesters', route: AcademicSemesterRoutes },
   { path: '/academic-faculties', route: AcademicFacultyRoutes },
   { path: '/academic-departments', route: AcademicDepartmentRoutes },
